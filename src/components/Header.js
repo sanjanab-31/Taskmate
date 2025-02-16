bca457b526d196524dd924b2d317a73eb93b1155
Sanju <sanjana.b0831@gmail.com>
2025-02-16 12:17:56 +0530

    added themeselector in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,14 @@ export const Header = () => {
             <img src={Logo} alt="Taskmate logo" />
             <span>Taskmate</span>
         </div>
+        <div className="themeSelector">
+          <span className="light "></span>
+          <span className="medium activeTheme"></span>
+          <span className="dark"></span>
+          <span className="gone"></span>
+          <span className="gTwo"></span>
+          <span className="gThree"></span>
+        </div>
     </header>
   )
 }
